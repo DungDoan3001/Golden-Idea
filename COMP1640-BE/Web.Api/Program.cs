cd24f993fee494acbd31ad79a0078d3304805667
DunDon <dungdtgcs200419@fpt.edu.vn>
2023-02-18 12:45:53 +0700

    Revert "Revert "implement logging service""
    This reverts commit f8d45adde5d488f769361226807d91e07ce76d98.

--- COMP1640-BE/Web.Api/Program.cs
+++ COMP1640-BE/Web.Api/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using NLog.Web;
 
 namespace Web.Api
 {
@@ -20,7 +22,10 @@ namespace Web.Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    .ConfigureLogging((hostingContext, logging) => {
+                        logging.AddNLog(hostingContext.Configuration.GetSection("Logging"));
+                    });
                 });
     }
 }
