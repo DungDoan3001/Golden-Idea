5587c185d2cf148073c5786788a4bf7729d86645
Thuan <thuan.bui@kyanon.digital>
2023-02-26 15:39:10 +0700

    update ResetPassword and add ResetPassword table

---
+++ COMP1640-BE/Web.Api/Entities/ResetPassword.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+
+namespace Web.Api.Entities
+{
+    public class ResetPassword
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid UserId { get; set; }
+        public string Token { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public virtual User User { get; set; }
+    }
+}
