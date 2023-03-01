eaf1505c185310bd8f28d473fe9ebe2a8bcbac9d
Harry Tran <trungtien2811@gmail.com>
2023-03-01 20:41:06 +0700

    Connect BE with FE for Login, Logout, ResetPass, Forgot pass

--- COMP1640-FE/client/src/app/models/User.ts
+++ COMP1640-FE/client/src/app/models/User.ts
@@ -1,13 +1,20 @@
 export interface User {
     id: string;
     name: string;
-    userName:string;
+    userName: string;
     email: string;
     password: string;
     address: string;
     phoneNumber: string,
-    avatarURL:string;
+    avatarURL: string;
     departmentId: string;
     role: string;
     token: string;
+}
+export interface UserLogin {
+    name: string;
+    emailaddress: string;
+    Avatar: string;
+    role: string[];
+    token?: string;
 }
--- COMP1640-FE/client/src/app/routes/RequireAuth.tsx
+++ COMP1640-FE/client/src/app/routes/RequireAuth.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useAppSelector } from "../store/configureStore";
+import { useEffect } from "react";
 
 interface Props {
     roles?: string[];
@@ -9,15 +10,16 @@ interface Props {
 export default function RequireAuth({ roles }: Props) {
     const { user } = useAppSelector(state => state.account);
     const location = useLocation();
-
+    useEffect(() => {
+        toast.error('Login to access this area');
+    }, []);
     if (!user) {
         return <Navigate to='/login' state={{ from: location }} />
     }
 
     if (roles && !roles.some(r => user.role?.includes(r))) {
-        toast.error('Not authorised to access this area');
+        toast.error('Not authorized to access this area');
         return <Navigate to='/login' />
     }
-
     return <Outlet />
 }
