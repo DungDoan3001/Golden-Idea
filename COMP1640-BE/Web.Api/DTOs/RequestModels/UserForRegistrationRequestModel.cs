476c2166fa018f0d95765b31e6c137178429cce2
Thuan <thuan.bui@kyanon.digital>
2023-03-01 13:09:27 +0700

    validata size file to 10mb

--- COMP1640-BE/Web.Api/DTOs/RequestModels/UserForRegistrationRequestModel.cs
+++ COMP1640-BE/Web.Api/DTOs/RequestModels/UserForRegistrationRequestModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
+using Web.Api.Extensions;
 
 namespace Web.Api.DTOs.RequestModels
 {
@@ -16,6 +17,9 @@ namespace Web.Api.DTOs.RequestModels
         public string Email { get; set; }
         [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
+
+        [DataType(DataType.Upload)]
+        [MaxFileSize(10 * 1024 * 1024)]
         public IFormFile File { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
---
+++ COMP1640-BE/Web.Api/Extensions/MaxFileSizeAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Api.Extensions
+{
+    public class MaxFileSizeAttribute : ValidationAttribute
+    {
+        private readonly int _maxFileSize;
+        public MaxFileSizeAttribute(int maxFileSize)
+        {
+            _maxFileSize = maxFileSize;
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                if (file.Length > _maxFileSize)
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+            }
+            return ValidationResult.Success;
+        }
+
+        public string GetErrorMessage()
+        {
+            return $"Maximum allowed file size is {_maxFileSize} bytes.";
+        }
+    }
+}
