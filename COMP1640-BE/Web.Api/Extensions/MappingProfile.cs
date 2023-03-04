c6a8a21d9a98856697daa7652c3321daae55f487
Thuan <thuan.bui@kyanon.digital>
2023-03-04 14:07:25 +0700

    fix response

--- COMP1640-BE/Web.Api/Extensions/MappingProfile.cs
+++ COMP1640-BE/Web.Api/Extensions/MappingProfile.cs
@@ -14,18 +14,18 @@ namespace Web.Api.Extensions
         {
             // Department
             CreateMap<Department, DepartmentResponseModel>()
-                .ForMember(dest => dest.DepartmentId, opt => opt.MapFrom(src => src.Id));
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id));
             CreateMap<DepartmentRequestModel, Department>();
 
             // Topic
             CreateMap<Entities.Topic, TopicResponseModel>()
-                .ForMember(dest => dest.TopicId, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.User.UserName));
             CreateMap<TopicRequestModel, Entities.Topic>();
 
             // Topic
             CreateMap<Entities.Category, CategoryResponseModel>()
-                .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.Id));
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id));
             CreateMap<CategoryRequestModel, Entities.Category>();
 
             // Role
@@ -33,7 +33,6 @@ namespace Web.Api.Extensions
 
             // Authentication
             CreateMap<UserForRegistrationRequestModel, User>();
-            CreateMap<User, UserForRegistrationResponseModel>();
 
             //User
             CreateMap<UserRequestModel, User>();
