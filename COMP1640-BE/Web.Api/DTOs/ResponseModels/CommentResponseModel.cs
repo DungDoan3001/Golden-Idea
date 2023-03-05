555e382884f9d2e9124d2a9dae900378176449e1
Thuan <thuan.bui@kyanon.digital>
2023-03-05 22:11:28 +0700

    update response comment

--- COMP1640-BE/Web.Api/DTOs/ResponseModels/CommentResponseModel.cs
+++ COMP1640-BE/Web.Api/DTOs/ResponseModels/CommentResponseModel.cs
@@ -5,6 +5,7 @@ namespace Web.Api.DTOs.ResponseModels
     public class CommentResponseModel
     {
         public Guid Id { get; set; }
+        public Guid IdeaId { get; set; }
         public string Content { get; set; }
         public string Username { get; set; }
         public bool IsAnonymous { get; set; }
