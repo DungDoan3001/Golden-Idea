e11dd7d8ec3ed2b89461aa39311731b2c88c4857
Bui Cong Thuan <thuanbui2611@gmail.com>
2023-03-07 19:19:11 +0700

    api get contributors

--- COMP1640-BE/Web.Api/Controllers/ChartController.cs
+++ COMP1640-BE/Web.Api/Controllers/ChartController.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Web.Api.DTOs.ResponseModels;
+using Web.Api.Services.Chart;
 
 namespace Web.Api.Controllers
 {
@@ -7,9 +13,33 @@ namespace Web.Api.Controllers
     [ApiController]
     public class ChartController : ControllerBase
     {
-        public ChartController()
+        public readonly IChartService _chartService;
+        public ChartController(IChartService chartService)
         {
+            _chartService = chartService;
+        }
+        /// <summary>
+        /// Get all needed information for Contributors Chart.
+        /// </summary>
+        /// <response code="200">Successfully get all information</response>
+        /// <response code="400">There is something wrong while execute.</response>
+        [HttpGet("contributors")]
+        public async Task<ActionResult<List<ContributorResponseModel>>> GetContributors() 
+        {
+            try
+            {
+                return await _chartService.GetContributor();
+            }
+            catch (Exception ex)
+            {
 
+                return BadRequest(new MessageResponseModel
+                {
+                    Message = "Error",
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Errors = new List<string> { ex.GetBaseException().Message }
+                });
+            }
         }
     }
 }
--- COMP1640-BE/Web.Api/Services/Chart/IChartService.cs
+++ COMP1640-BE/Web.Api/Services/Chart/IChartService.cs
@@ -1,6 +1,11 @@
-﻿namespace Web.Api.Services.Chart
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Web.Api.DTOs.ResponseModels;
+
+namespace Web.Api.Services.Chart
 {
     public interface IChartService
     {
+        Task<List<ContributorResponseModel>> GetContributor();
     }
 }
