cc75059c6a77e8bf85e6844ff00a9617af8a3308
Bui Cong Thuan <thuanbui2611@gmail.com>
2023-03-07 17:52:37 +0700

    chart controller

---
+++ COMP1640-BE/Web.Api/Controllers/ChartController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Api.Controllers
+{
+    [Route("api/charts")]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        public ChartController()
+        {
+
+        }
+    }
+}
