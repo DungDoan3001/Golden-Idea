5ae224577b35253ef26c47b8b017db6e71c2f786
Hiepphgcs200618 <hiepphgcs200618@fpt.edu.vn>
2023-03-15 19:43:12 +0700

    Fix bug in idea Detail

--- COMP1640-FE/client/src/pages/ideaDetail/index.tsx
+++ COMP1640-FE/client/src/pages/ideaDetail/index.tsx
@@ -59,157 +59,148 @@ const IdeaDetail = () => {
   return (
     <>
       {loading ? <Loading /> :
-        <Box alignItems="center" justifyContent="center"
-          width="100%"
-          sx={{
-            [theme.breakpoints.up('sm')]: {
-              width: '80%',
-              m: "1rem 6rem",
-            },
-            [theme.breakpoints.down('sm')]: {
-              width: '110%',
-              m: "1rem 2rem",
-            },
-          }}
-        >
-          <Typography
-            m="2rem 0rem"
-            variant="h1"
-            color={theme.palette.content.main}
-            fontWeight="bold"
+        <>
+          <Box alignItems="center" justifyContent="center"
+            width="100%"
             sx={{
-              textAlign: 'center',
-              textTransform: 'uppercase',
-              position: 'relative',
-              display: 'inline-block',
-              '&:after': {
-                content: '""',
-                position: 'absolute',
-                top: '-10px',
-                left: '-10px',
-                right: '-10px',
-                bottom: '-10px',
-                border: `2px solid ${theme.palette.secondary.main}`,
-                borderStyle: 'dashed',
-                borderRadius: '10px',
+              [theme.breakpoints.up('sm')]: {
+                width: '80%',
+                m: "1rem 6rem",
+              },
+              [theme.breakpoints.down('sm')]: {
+                width: '110%',
+                m: "1rem 2rem",
               },
             }}
           >
-            {idea?.topic.name}
-          </Typography>
-          <Box sx={{ backgroundColor: theme.palette.comment.main, borderRadius: "0.5rem" }}>
-            <Box p="1rem 5%">
-              <Grid
-                display="bottom" alignItems="center" justifyContent="bottom"
-                container spacing={1} columns={{ xs: 4, sm: 8, md: 12 }}
-              >
-                <Grid item xs={3} sm={4} md={6}>
-                  {/* <PostAuthorInfo top={false} data={idea} /> */}
-                </Grid>
-              </Grid>
-              <Typography
-                textAlign="left"
-                mb="1rem"
-                variant="h2"
-                color={theme.palette.content.main}
-                fontWeight="bold"
-              >
-                {idea?.title}
-              </Typography>
-              <Box m="2rem">
-                <Box
-                  component="img"
-                  alt="thumbnail"
-                  src={idea?.image}
-                  height="100%"
-                  width="100%"
+            <Typography
+              m="2rem 0rem"
+              variant="h1"
+              color={theme.palette.content.main}
+              fontWeight="bold"
+              sx={{
+                textAlign: 'center',
+                textTransform: 'uppercase',
+                position: 'relative',
+                display: 'inline-block',
+                '&:after': {
+                  content: '""',
+                  position: 'absolute',
+                  top: '-10px',
+                  left: '-10px',
+                  right: '-10px',
+                  bottom: '-10px',
+                  border: `2px solid ${theme.palette.secondary.main}`,
+                  borderStyle: 'dashed',
+                  borderRadius: '10px',
+                },
+              }}
+            >
+              {idea?.topic.name}
+            </Typography>
+            <Box sx={{ backgroundColor: theme.palette.comment.main, borderRadius: "0.5rem" }}>
+              <Box p="1rem 5%">
+                <Grid
+                  display="bottom" alignItems="center" justifyContent="bottom"
+                  container spacing={1} columns={{ xs: 4, sm: 8, md: 12 }}
                 >
+                  <Grid item xs={3} sm={4} md={6}>
+                    {/* <PostAuthorInfo top={false} data={idea} /> */}
+                  </Grid>
+                </Grid>
+                <Typography
+                  textAlign="left"
+                  mb="1rem"
+                  variant="h2"
+                  color={theme.palette.content.main}
+                  fontWeight="bold"
+                >
+                  {idea?.title}
+                </Typography>
+                <Box m="2rem">
+                  <Box
+                    component="img"
+                    alt="thumbnail"
+                    src={idea?.image}
+                    height="100%"
+                    width="100%"
+                  >
+                  </Box>
                 </Box>
-              </Box>
-              <Typography
-                textAlign="justify"
-                variant="h5"
-                color={theme.palette.content.main}
-                fontSize="1rem"
-              >
-                {idea?.content}
-              </Typography>
-              <Box m="1rem 0rem">
-                <IconButton onClick={ClickLike}>
-                  {(isLike) ?
-                    <ThumbUpIcon
-                      style={{
-                        fontSize: "1.5rem",
-                        color: theme.palette.content.main,
-                        paddingRight: "0.5rem"
-                      }}
-                    /> :
-                    <ThumbUpTwoToneIcon
-                      style={{
-                        fontSize: "1.5rem",
-                        color: theme.palette.content.icon,
-                        paddingRight: "0.5rem"
-                      }}
-                    />
-                  }
-                  <Box fontSize="1rem">
-                    {idea?.upVote}
-                  </Box>
-                </IconButton>
-                <IconButton onClick={ClickDislike}>
-                  {(isDislike) ?
-                    <ThumbDownIcon
-                      style={{
-                        fontSize: "1.5rem",
-                        color: theme.palette.content.main,
-                        paddingRight: "0.5rem"
-                      }}
-                    /> :
-                    <ThumbDownTwoToneIcon
-                      style={{
-                        fontSize: "1.5rem",
-                        color: theme.palette.content.icon,
-                        paddingRight: "0.5rem"
-                      }}
-                    />
-                  }
+                <Typography
+                  textAlign="justify"
+                  variant="h5"
+                  color={theme.palette.content.main}
+                  fontSize="1rem"
+                >
+                  {idea?.content}
+                </Typography>
+                <Box m="1rem 0rem">
+                  <IconButton onClick={ClickLike}>
+                    {(isLike) ?
+                      <ThumbUpIcon
+                        style={{
+                          fontSize: "1.5rem",
+                          color: theme.palette.content.main,
+                          paddingRight: "0.5rem"
+                        }}
+                      /> :
+                      <ThumbUpTwoToneIcon
+                        style={{
+                          fontSize: "1.5rem",
+                          color: theme.palette.content.icon,
+                          paddingRight: "0.5rem"
+                        }}
+                      />
+                    }
+                    <Box fontSize="1rem">
+                      {idea?.upVote}
+                    </Box>
+                  </IconButton>
+                  <IconButton onClick={ClickDislike}>
+                    {(isDislike) ?
+                      <ThumbDownIcon
+                        style={{
+                          fontSize: "1.5rem",
+                          color: theme.palette.content.main,
+                          paddingRight: "0.5rem"
+                        }}
+                      /> :
+                      <ThumbDownTwoToneIcon
+                        style={{
+                          fontSize: "1.5rem",
+                          color: theme.palette.content.icon,
+                          paddingRight: "0.5rem"
+                        }}
+                      />
+                    }
 
-                  <Box fontSize="1rem">
-                    {idea?.downVote}
-                  </Box>
-                </IconButton>
-                <IconButton disabled>
-                  <ChatBubbleTwoToneIcon
-                    style={{
-                      fontSize: "1.5rem",
-                      color: theme.palette.content.icon,
-                      paddingRight: "0.5rem"
-                    }}
-                  />
-                  <Box fontSize="1rem">
-                    12
-                  </Box>
-                </IconButton>
+                    <Box fontSize="1rem">
+                      {idea?.downVote}
+                    </Box>
+                  </IconButton>
+                  <IconButton disabled>
+                    <ChatBubbleTwoToneIcon
+                      style={{
+                        fontSize: "1.5rem",
+                        color: theme.palette.content.icon,
+                        paddingRight: "0.5rem"
+                      }}
+                    />
+                    <Box fontSize="1rem">
+                      12
+                    </Box>
+                  </IconButton>
+                </Box>
               </Box>
             </Box>
           </Box>
-        ))}
-      </Box>
-      <Typography
-        m="1rem 0rem"
-        variant="h3"
-        color={theme.palette.content.main}
-        fontWeight="semibold"
-      >
-        Comment
-      </Typography>
-      <Divider variant="fullWidth" />
-      <Comment ideaId='8bc9ff66-f901-44da-b9e3-294e0de0ac15' />
-    </Box >
+          <Divider variant="fullWidth" />
+          <Comment ideaId='8bc9ff66-f901-44da-b9e3-294e0de0ac15' />
+        </>
       }
     </>
   );
-
 }
 
 export default IdeaDetail
