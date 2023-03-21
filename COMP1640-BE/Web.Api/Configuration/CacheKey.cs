d809b8652a48e1eb0f6b445f57b42bf7226e8e3a
Bui Cong Thuan <thuanbui2611@gmail.com>
2023-03-22 00:04:57 +0700

    fix bug

--- COMP1640-BE/Web.Api/Configuration/CacheKey.cs
+++ COMP1640-BE/Web.Api/Configuration/CacheKey.cs
@@ -5,33 +5,33 @@
         public class TopicCacheKey
         {
             public string GetAllCacheKey { get; set; } = "GetAllTopicCacheKey";
-            public string GetByUserIdCacheKey { get; set; } = "GetTopicByUserIdCacheKey";
-            public string GetByIdCacheKey { get; set; } = "GetTopicByIdCacheKey";
+            //public string GetByUserIdCacheKey { get; set; } = "GetTopicByUserIdCacheKey";
+            //public string GetByIdCacheKey { get; set; } = "GetTopicByIdCacheKey";
         }
         public class UserCacheKey
         {
             public string GetAllCacheKey { get; set; } = "GetAllUserCacheKey";
             public string GetAllStaffCacheKey { get; set; } = "GetAllStaffCacheKey";
             public string GetAllAdminQACacheKey { get; set; } = "GetAllAdminQACacheKey";
-            public string GetByIdCacheKey { get; set; } = "GetUserByIdCacheKey";
+            //public string GetByIdCacheKey { get; set; } = "GetUserByIdCacheKey";
         }
         public class CategoryCacheKey
         {
             public string GetAllCacheKey { get; set; } = "GetAllCategoryCacheKey";
-            public string GetByIdCacheKey { get; set; } = "GetByIdCategoryCacheKey";
+            //public string GetByIdCacheKey { get; set; } = "GetByIdCategoryCacheKey";
         }
         public class DepartmentCacheKey
         {
             public string GetAllCacheKey { get; set; } = "GetAllDepartmentCacheKey";
-            public string GetByIdCacheKey { get; set; } = "GetByIdDepartmentCacheKey";
+            //public string GetByIdCacheKey { get; set; } = "GetByIdDepartmentCacheKey";
         }
         public class IdeaCacheKey
         {
             public string GetAllCacheKey { get; set; } = "GetAllIdeaCacheKey";
-            public string GetAllByAuthorCacheKey { get; set; } = "GetAllIdeaByAuthorCacheKey";
-            public string GetByIdCacheKey { get; set; } = "GetIdeaByIdCacheKey";
-            public string GetBySlugCacheKey { get; set; } = "GetIdeaBySlugCacheKey";
-            public string GetByTopicCacheKey { get; set; } = "GetIdeaByTopicCacheKey";
+            //public string GetAllByAuthorCacheKey { get; set; } = "GetAllIdeaByAuthorCacheKey";
+            //public string GetByIdCacheKey { get; set; } = "GetIdeaByIdCacheKey";
+            //public string GetBySlugCacheKey { get; set; } = "GetIdeaBySlugCacheKey";
+            //public string GetByTopicCacheKey { get; set; } = "GetIdeaByTopicCacheKey";
         }
     }
 }
